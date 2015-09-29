1579d18f5bdf8cde8dc6de22b5b1dd4cc4987d31
夜 <540961620@qq.com>
2015-09-29 16:12:49 +0800

    将私有函数提前

--- jquery.bs_label.js
+++ jquery.bs_label.js
@@ -1,113 +1,6 @@
 (function (jQuery) {
     var versions = "0.1";
     var pluginName = "jQuery.bs_label";
-    /* public methods ------------------------------------------------------- */
-    var methods = {
-        /**
-         *    $('#a').bs_label({
-*        //label数据
-*        "data": [
-*            {"val": "a", "name": "a", "type": "default"},
-*            {"val": "b", "name": "b", "type": "primary"},
-*            {"val": "c", "name": "c", "type": "success"},
-*            {"val": "d", "name": "d ", "type": "info"},
-*            {"val": "e", "name": "e", "type": "warning"},
-*            {"val": "f", "name": "f", "type": "danger"}
-*        ],
-*        //加载后自动点击的label的val
-*        "clickVal":"a",
-*        //点击后的回调
-*        "callBack": function (e, dName, dVal) {
-*            console.log(arguments)
-*        }
-*    });
-         */
-        init: function (options) {
-            var $elem = $(this);
-            var tagId = $elem.attr('id');
-            if (typeof tagId === "undefined") {
-                $.error('元素必须具有ID');
-                return false;
-            }
-            var data = options.data;
-            var callBack = options.callBack;
-            var clickVal = options.clickVal;
-            if (typeof data === "undefined") {
-                $.error('没有传入data');
-                return false;
-            }
-            if (data.length == 0) {
-                $.error('data为空');
-                return false;
-            }
-            if (typeof callBack === "undefined") {
-                $.error('没有传入callBack');
-                return false;
-            }
-            putElem(tagId, $elem);
-            putCallBack(tagId, callBack);
-            putDisabled(tagId, false);
-
-            $elem.html(craftingTable(data));
-            $elem.addClass("clearfix");
-            $elem.unbind();
-            $elem.find('button').unbind('click');
-            $elem.find('button').on('click', click);
-            if (typeof clickVal !== "undefined") {
-                methods.checked.apply(this, [clickVal]);
-            }
-
-            return $elem;
-        },
-        /**
-         * 选中一个tag
-         * @param val tag的val
-         * @returns {*}
-         */
-        checked: function (val) {
-            var $elem = $(this);
-            var id = $elem.attr('id');
-            if (typeof id === "undefined") {
-                $.error('元素必须具有ID');
-                return false;
-            }
-            var $ele = getElem(id);
-            if (typeof $ele !== "object") {
-                $.error('Id:' + id + '并没有被' + pluginName + '接管');
-                return false;
-            }
-            var $tag = $ele.find('[data-val="' + val + '"]');
-            if ($tag.length === 0) {
-                $.error('val:"' + val + '"在Id:"' + id + '"中不存在');
-                return false;
-            }
-            click.apply(this, [pluginName + '.code.checked', $tag]);
-            return $elem;
-        },
-        disable: function (boolean) {
-            var $elem = $(this);
-            var id = $elem.attr('id');
-            if (typeof id === "undefined") {
-                $.error('元素必须具有ID');
-                return false;
-            }
-            var $ele = getElem(id);
-            if (typeof $ele !== "object") {
-                $.error('Id:' + id + '并没有被' + pluginName + '接管');
-                return false;
-            }
-            if (typeof boolean === "boolean") {
-                putDisabled(id, boolean);
-            } else {
-                putDisabled(id, !getDisabled(id));
-            }
-            disabledLabel(id);
-        },
-        ver: function () {
-            return versions;
-        }
-    };
-
     /* private methods ------------------------------------------------------ */
 
     var elemPool = {};
@@ -190,5 +83,111 @@
             $.error("方法 " + method + "不存在于" + pluginName);
         }
 
+    };
+    /* public methods ------------------------------------------------------- */
+    var methods = {
+        /**
+         *    $('#a').bs_label({
+*        //label数据
+*        "data": [
+*            {"val": "a", "name": "a", "type": "default"},
+*            {"val": "b", "name": "b", "type": "primary"},
+*            {"val": "c", "name": "c", "type": "success"},
+*            {"val": "d", "name": "d ", "type": "info"},
+*            {"val": "e", "name": "e", "type": "warning"},
+*            {"val": "f", "name": "f", "type": "danger"}
+*        ],
+*        //加载后自动点击的label的val
+*        "clickVal":"a",
+*        //点击后的回调
+*        "callBack": function (e, dName, dVal) {
+*            console.log(arguments)
+*        }
+*    });
+         */
+        init: function (options) {
+            var $elem = $(this);
+            var tagId = $elem.attr('id');
+            if (typeof tagId === "undefined") {
+                $.error('元素必须具有ID');
+                return false;
+            }
+            var data = options.data;
+            var callBack = options.callBack;
+            var clickVal = options.clickVal;
+            if (typeof data === "undefined") {
+                $.error('没有传入data');
+                return false;
+            }
+            if (data.length === 0) {
+                $.error('data为空');
+                return false;
+            }
+            if (typeof callBack === "undefined") {
+                $.error('没有传入callBack');
+                return false;
+            }
+            putElem(tagId, $elem);
+            putCallBack(tagId, callBack);
+            putDisabled(tagId, false);
+
+            $elem.html(craftingTable(data));
+            $elem.addClass("clearfix");
+            $elem.unbind();
+            $elem.find('button').unbind('click');
+            $elem.find('button').on('click', click);
+            if (typeof clickVal !== "undefined") {
+                methods.checked.apply(this, [clickVal]);
+            }
+
+            return $elem;
+        },
+        /**
+         * 选中一个tag
+         * @param val tag的val
+         * @returns {*}
+         */
+        checked: function (val) {
+            var $elem = $(this);
+            var id = $elem.attr('id');
+            if (typeof id === "undefined") {
+                $.error('元素必须具有ID');
+                return false;
+            }
+            var $ele = getElem(id);
+            if (typeof $ele !== "object") {
+                $.error('Id:' + id + '并没有被' + pluginName + '接管');
+                return false;
+            }
+            var $tag = $ele.find('[data-val="' + val + '"]');
+            if ($tag.length === 0) {
+                $.error('val:"' + val + '"在Id:"' + id + '"中不存在');
+                return false;
+            }
+            click.apply(this, [pluginName + '.code.checked', $tag]);
+            return $elem;
+        },
+        disable: function (boolean) {
+            var $elem = $(this);
+            var id = $elem.attr('id');
+            if (typeof id === "undefined") {
+                $.error('元素必须具有ID');
+                return false;
+            }
+            var $ele = getElem(id);
+            if (typeof $ele !== "object") {
+                $.error('Id:' + id + '并没有被' + pluginName + '接管');
+                return false;
+            }
+            if (typeof boolean === "boolean") {
+                putDisabled(id, boolean);
+            } else {
+                putDisabled(id, !getDisabled(id));
+            }
+            disabledLabel(id);
+        },
+        v: function () {
+            return versions;
+        }
     };
 })(jQuery);
